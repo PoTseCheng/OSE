7d38a9bb64b5c032860f0782aeecaad36c525497
PoTseCheng <potsecheng@gmail.com>
2020-11-19 22:03:11 +0100

    Update function y.

--- GSS/Gss python/tools/auxiliary.py
+++ GSS/Gss python/tools/auxiliary.py
@@ -4,11 +4,13 @@ def reading(x):
     '''
     reading the csv files
     '''
-    df=pd.read_csv(str(x))
+    df=pd.read_csv(x)
     add=df.columns.tolist()
     df.loc[-1] = add 
     df.index = df.index + 1  
     df = df.sort_index()
-    df.columns=[str(x).replace(".csv","")]
+    df.columns=[x.replace(".csv","")]
 
     return df
+
+
